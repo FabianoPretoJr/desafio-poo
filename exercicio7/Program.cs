51bbec69c6e602edb275a267507a5dda87526bfd
foeu <fabiano.junior@gft.com>
2020-10-18 12:04:15 -0300

    Exercicio 7 funcionando

--- exercicio7/Program.cs
+++ exercicio7/Program.cs
@@ -6,7 +6,13 @@ namespace exercicio7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Gerente g = new Gerente("Paula", 50, 8000);
+            Supervisor s = new Supervisor("Pedro", 34, 5000);
+            Vendedor v = new Vendedor("Carlos", 29, 4000);
+
+            Console.WriteLine("Salário + bonificação = " + g.Bonificar());
+            Console.WriteLine("Salário + bonificação = " + s.Bonificar());
+            Console.WriteLine("Salário + bonificação = " + v.Bonificar());
         }
     }
 }
